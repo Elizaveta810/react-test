a468810c875b17a61418d1ff5e7191697719387d
Elizaveta810 <elizaveta.k810@mail.ru>
2024-02-04 18:35:49 +0300

    распределение карточек

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,18 +1,20 @@
 import Card from "../Card/Card";
 
-function Column({title}) {
+function Column({title, cardList}) {
   return (
     <div className="main__column column">
       <div className="column__title">
         <p>{title}</p>
       </div>
       <div className="cards">
-        
-        <Card topic={"Web design"} title={"Новая задача"}/>
+        {cardList.map((card) => 
+          <Card topic={card.theme} title={card.title} key={card.id} />
+        )}
+        {/*<Card topic={"Web design"} title={"Новая задача"}/>
         <Card topic={"Research"} title={"Новая задача"}/>
         <Card topic={"Web design"} title={"Новая задача"}/>
         <Card topic={"Research"} title={"Новая задача"}/>
-        <Card topic={"Web design"} title={"Новая задача"}/>
+  <Card topic={"Web design"} title={"Новая задача"}/>*/}
       </div>
     </div>
   );
