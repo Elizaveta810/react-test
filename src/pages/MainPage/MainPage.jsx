5b4a89bd313464539a8c11b4c6c96f7c98df4149
Elizaveta810 <elizaveta.k810@mail.ru>
2024-02-22 22:08:41 +0300

    главная страница

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import { cardList } from "../../date";
+import PopExit from "../../components/popups/PopExit/PopExit";
+import PopBrowse from "../../components/popups/PopBrowse/PopBrowse";
+import PopNewCard from "../../components/popups/PopNewCard/PopNewCard";
+import Header from "../../components/Header/Header";
+import MainContent from "../../components/MainContent/MainContent";
+import Column from "../../components/Column/Column";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+
+
+
+
+
+const statusList = [
+  "Без статуса",
+  "Нужно сделать",
+  "В работе",
+  "Тестирование",
+  "Готово",
+];
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [cards, setCards] = useState(cardList);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // 2 секунды задержки
+  }, []); // Пустой массив зависимостей для запуска только при монтировании компонента
+
+  function addCard() {
+    // Логика добавления карточки
+    const newCard = {
+      id: cards.length + 1,
+      theme: "Web Design",
+      title: "Название задачи",
+      date: "30.10.23",
+      status: "Без статуса",
+    };
+    setCards([...cards, newCard]);
+  }
+
+  return (
+    <>
+      <div className="wrapper">
+        <PopExit />
+        <PopBrowse />
+        <PopNewCard />
+
+        <Header addCard={addCard} />
+        {isLoading ? (
+          "Пожалуйста подождите, идет загрузка..."
+        ) : (
+          <MainContent>
+            {statusList.map((status) => (
+              <Column
+                title={status}
+                key={status}
+                cardList={cards.filter((card) => card.status === status)}
+              />
+            ))}
+          </MainContent>
+        )}
+      </div>
+
+      <div>
+        <a href="https://vitejs.dev">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
