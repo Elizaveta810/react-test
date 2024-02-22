c924058d4cc6f674bf2352fa2c25b7669c61db7a
Elizaveta810 <elizaveta.k810@mail.ru>
2024-02-22 11:26:01 +0300

    Сщздали ф-ии страниц входа и регистрации

---
+++ src/pages/SigninPage/SigninPage.jsx
@@ -0,0 +1,42 @@
+function SigninPage() {
+  return (
+    <body>
+      <div className="wrapper">
+        <div className="container-signin">
+          <div className="modal">
+            <div className="modal__block">
+              <div className="modal__ttl">
+                <h2>Вход</h2>
+              </div>
+              <form className="modal__form-login" id="formLogIn" action="#">
+                <input
+                  className="modal__input"
+                  type="text"
+                  name="login"
+                  id="formlogin"
+                  placeholder="Эл. почта"
+                ></input>
+                <input
+                  className="modal__input"
+                  type="password"
+                  name="password"
+                  id="formpassword"
+                  placeholder="Пароль"
+                ></input>
+                <button className="modal__btn-enter _hover01" id="btnEnter">
+                  <a href="../main.html">Войти</a>
+                </button>
+                <div className="modal__form-group">
+                  <p>Нужно зарегистрироваться?</p>
+                  <a href="signup.html">Регистрируйтесь здесь</a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+}
+
+export default SigninPage;
