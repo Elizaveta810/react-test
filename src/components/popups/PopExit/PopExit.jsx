028baccc59978b23cbbe8688753113298aea8882
Elizaveta810 <elizaveta.k810@mail.ru>
2024-02-25 00:31:28 +0300

    кнопка ДА ВЫЙТИ

--- src/components/popups/PopExit/PopExit.jsx
+++ src/components/popups/PopExit/PopExit.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import { appRoutes } from "../../../lib/appRoutes";
+
 function PopExit() {
   return (
     <div className="pop-exit" id="popExit">
@@ -8,9 +11,11 @@ function PopExit() {
           </div>
           <form className="pop-exit__form" id="formExit" action="#">
             <div className="pop-exit__form-group">
-              <button className="pop-exit__exit-yes _hover01" id="exitYes">
-                <a href="modal/signin.html">Да, выйти</a>{" "}
-              </button>
+              <Link to={appRoutes.SIGNIN}>
+              <span className="pop-exit__exit-yes _hover01" id="exitYes">
+                Да, выйти
+              </span>
+              </Link>
               <button className="pop-exit__exit-no _hover03" id="exitNo">
                 <a href="main.html">Нет, остаться</a>{" "}
               </button>
