fe907b862428b181c8227c0115f0a00828b3d465
Elizaveta810 <elizaveta.k810@mail.ru>
2024-03-25 12:34:39 +0300

    ф-я изменения задачи

--- src/api.js
+++ src/api.js
@@ -1,11 +1,9 @@
-
 export let token;
 export const setToken = (newToken) => {
-    token = newToken;
+  token = newToken;
 };
-const baseHost ="https://wedev-api.sky.pro/api/kanban";
+const baseHost = "https://wedev-api.sky.pro/api/kanban";
 const userHost = "https://wedev-api.sky.pro/api/user";
-
 
 //Получить список задач
 export async function getTodos({ token }) {
@@ -23,51 +21,83 @@ export async function getTodos({ token }) {
   return data;
 }
 
-
 //Добавить задачу в список
 
-export async function postTodo({taskData, token}) {
+export async function postTodo({ taskData, token }) {
   const response = await fetch(baseHost, {
     method: "POST",
     headers: {
-Authorization: `Bearer ${token}`,
-      },
-        body: JSON.stringify({
-        "title": taskData.title,
-        "topic": taskData.topic,
-        "status": "Без статуса",
-        "description": taskData.description,
-        "date": taskData.date,
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      title: taskData.title,
+      topic: taskData.topic,
+      status: "Без статуса",
+      description: taskData.description,
+      date: taskData.date,
     }),
   });
   if (!response.status === 400) {
-      throw new Error("Заполнены не все поля");
+    throw new Error("Заполнены не все поля");
   }
   const data = await response.json();
   return data;
 }
 
+// Изменение задачи
+export async function changeTask({
+  title,
+  topic,
+  status,
+  description,
+  date,
+  _id,
+}) {
+  const response = await fetch(baseHost + "/" + _id, {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      title,
+      topic,
+      status,
+      description,
+      date,
+    }),
+  });
 
-//РЕГИСТРАЦИЯ
-export function signUp({ login, name, password }) {
-  return fetch(userHost, {
-   method: "POST",
-   body: JSON.stringify({ login, name, password }),
- }).then((response) => {
-   if (response.status === 400) {
-     throw new Error("Такой пользователь уже существует");
-   }
-   return response.json();
- });
-
- 
+  if (response.status === 201) {
+    const data = await response.json();
+    return data;
+  } else {
+    throw new Error("Не удалось редактировать задачу, попробуйте снова");
+  }
 }
 
 
 
+
+
+
+
+
+//РЕГИСТРАЦИЯ
+export function signUp({ login, name, password }) {
+  return fetch(userHost, {
+    method: "POST",
+    body: JSON.stringify({ login, name, password }),
+  }).then((response) => {
+    if (response.status === 400) {
+      throw new Error("Такой пользователь уже существует");
+    }
+    return response.json();
+  });
+}
+
 //АВТОРИЗАЦИЯ
-export default  function signIn({ login, password }) {
-   return fetch(userHost + "/login", {
+export default function signIn({ login, password }) {
+  return fetch(userHost + "/login", {
     method: "POST",
     body: JSON.stringify({ login, password }),
   }).then((response) => {
@@ -76,8 +106,6 @@ export default  function signIn({ login, password }) {
     }
     return response.json();
   });
- 
-  
 }
 
 
